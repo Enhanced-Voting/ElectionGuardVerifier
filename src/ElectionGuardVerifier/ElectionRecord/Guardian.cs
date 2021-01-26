cbc96a629714cc5caa12c20a6fcb023f2d525307
Aaron Wilson <wilsoaa@gmail.com>
2021-01-26 12:03:48 -0500

    Initial

---
+++ src/ElectionGuardVerifier/ElectionRecord/Guardian.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace ElectionGuardVerifier.ElectionRecord
+{
+    public class Guardian
+    {
+        public BigInteger PublicKey_K { get; set; }
+
+        public BigInteger Commitment_h { get; set; }
+
+        public BigInteger Challenge_c { get; set; }
+
+        public BigInteger Response_u { get; set; }
+    }
+}
---
+++ src/ElectionGuardVerifier/Verifiers/Verifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectionGuardVerifier.Verifiers
+{
+    public abstract class Verifier
+    {
+        protected Calculator Calculator { get; set; }
+
+        public Verifier() {
+            Calculator = new Calculator();
+        }
+
+    }
+}
